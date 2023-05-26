96d2e12c4149a6b6d4b4dc3b82d7f15e008b432e
MarianoReyes <josemarianoreyesh02@gmail.com>
2023-05-26 12:32:41 -0600

    arreglos imagen, y main

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+from neo4j import GraphDatabase
 
 app = Flask(__name__)
 
+aura_url = "bolt://neo4j_uri"
+user = "neo4j_user"
+password = "neo4j_password"
+
+# Cambia esto por tu propia clave secreta
+app.config['SECRET_KEY'] = 'tu_clave_secreta'
+driver = GraphDatabase.driver(aura_url, auth=(user, password))
+
+
 @app.route('/')
 def login_register():
-    return render_template('inicio.html')
+    return render_template('/login.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
